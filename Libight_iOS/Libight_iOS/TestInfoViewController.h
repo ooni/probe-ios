1ea6681e5d607102192a5dcf98160c03122d1160
Lorenzo Primiterra <lprimiterra@yahoo.it>
2015-04-14 17:43:43 +0200

    Add assets for test information and app icon

---
+++ Libight_iOS/Libight_iOS/TestInfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestInfoViewController.h
+//  Libight_iOS
+//
+//  Created by Lorenzo Primiterra on 14/04/15.
+//  Copyright (c) 2015 Simone Basso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestInfoViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet NSString *fileName;
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
+
