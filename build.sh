13488d48ddc4efa65996b15a0cfe9e2f3a4921ad
Lorenzo Primiterra <lprimiterra@yahoo.it>
2015-10-02 14:59:28 +0200

    Update measurement-kit subrepo

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 (cd measurement-kit/mobile/ios && ./scripts/build.sh)
-cp -Rf measurement-kit/mobile/ios/*.framework Libight_iOS/Libight_iOS/Frameworks/
+rm -Rf Libight_iOS/Libight_iOS/Frameworks/*.framework
+mv measurement-kit/mobile/ios/Frameworks/*.framework Libight_iOS/Libight_iOS/Frameworks/
