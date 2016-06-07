4db2a440af4690ee850e8d7dd98fabc6b269348d
Simone Basso <bassosimone@gmail.com>
2016-06-08 00:46:52 +0200

    NetworkMeasurement.mm: update to v0.2.0-beta
    1) make sure we use new API
    
    2) transition from setting the DNS once at the beginning to
       reading and setting it for each test that needs it
    
       (I am not sure whether dns_injection needs it, perhaps
        it actually does for ancillary operations?)
    
    3) reindent the code
    
    4) make sure we use correct backends

--- NetProbe/Model/NetworkMeasurement.mm
+++ NetProbe/Model/NetworkMeasurement.mm
@@ -14,41 +14,39 @@
 static void setup_idempotent() {
     static bool initialized = false;
     if (!initialized) {
-
         // Set the logger verbose and make sure it logs on the "logcat"
-        mk::set_verbose(1);
+        mk::set_verbosity(MK_LOG_DEBUG2);
         // XXX Ok to call NSLog() from another thread?
-        mk::on_log([](const char *s) {
+        mk::on_log([](uint32_t, const char *s) {
             NSLog(@"%s", s);
         });
-
-        // Copy DNS resolver(s) from device
-        // This code copied from the iOS tutorial
-        // XXX This should run before every test but to do this we need
-        // to further change measurement-kit DNS code
-        res_state res = nullptr;
-        do {
-            res = (res_state) malloc(sizeof(struct __res_state));
-            if (res == nullptr) break;
-            if (res_ninit(res) != 0) break;
-            mk::clear_nameservers();
-            for (int i = 0; i < res->nscount; ++i) {
-                char addr[INET_ADDRSTRLEN];
-                if (inet_ntop(AF_INET, &res->nsaddr_list[i].sin_addr, addr,
-                            sizeof (addr)) == nullptr) {
-                    continue;
-                }
-                mk::add_nameserver(addr);
-                NSLog(@"adding DNS resolver: %s", addr);
-            }
-            free(res);
-            res = nullptr;
-        } while (0);
-        if (res) free(res);
-
-        // Remember that we have initialized
         initialized = true;
     }
+}
+
+static std::string get_dns_server() {
+    std::string dns_server = "8.8.4.4:53";
+    res_state res = nullptr;
+    res = (res_state) malloc(sizeof(struct __res_state));
+    if (res == nullptr) {
+        return dns_server;
+    }
+    if (res_ninit(res) != 0) {
+        free(res);
+        return dns_server;
+    }
+    for (int i = 0; i < res->nscount; ++i) {
+        char addr[INET_ADDRSTRLEN];
+        if (inet_ntop(AF_INET, &res->nsaddr_list[i].sin_addr, addr,
+                      sizeof (addr)) == nullptr) {
+            continue;
+        }
+        NSLog(@"found DNS resolver: %s", addr);
+        dns_server = addr;
+        break;
+    }
+    free(res);
+    return dns_server;
 }
 
 @implementation NetworkMeasurement
@@ -87,10 +85,10 @@ static void setup_idempotent() {
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"hosts" ofType:@"txt"];
     mk::ooni::DnsInjectionTest()
-        .set_backend("8.8.8.8:53")
+        .set_options("backend", "8.8.8.1:53")
         .set_input_file_path([path UTF8String])
-        .set_verbose()
-        .on_log([self](const char *s) {
+        .set_verbosity(MK_LOG_DEBUG2)
+        .on_log([self](uint32_t, const char *s) {
             NSString *current = [NSString stringWithFormat:@"%@: %@", [super getDate], [NSString stringWithUTF8String:s]];
             NSLog(@"%s", s);
             dispatch_async(dispatch_get_main_queue(), ^{
@@ -120,24 +118,25 @@ static void setup_idempotent() {
 -(void) run {
     setup_idempotent();
     mk::ooni::HttpInvalidRequestLineTest()
-    .set_backend("http://www.google.com/")
-    .set_verbose()
-    .on_log([self](const char *s) {
-        // XXX OK to send messages to object from another thread?
-        NSString *current = [NSString stringWithFormat:@"%@: %@", [super getDate],
-                             [NSString stringWithUTF8String:s]];
-        NSLog(@"%s", s);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self.logLines addObject:current];
+        .set_options("backend", "http://213.138.109.232/")
+        .set_options("dns/nameserver", get_dns_server())
+        .set_verbosity(MK_LOG_DEBUG2)
+        .on_log([self](uint32_t, const char *s) {
+            // XXX OK to send messages to object from another thread?
+            NSString *current = [NSString stringWithFormat:@"%@: %@", [super getDate],
+                                 [NSString stringWithUTF8String:s]];
+            NSLog(@"%s", s);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.logLines addObject:current];
+            });
+        })
+        .run([self]() {
+            NSLog(@"http_invalid_request_line testEnded");
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.finished = TRUE;
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:nil];
+            });
         });
-    })
-    .run([self]() {
-        NSLog(@"http_invalid_request_line testEnded");
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.finished = TRUE;
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:nil];
-        });
-    });
 }
 
 @end
@@ -152,29 +151,28 @@ static void setup_idempotent() {
 
 -(void) run {
     setup_idempotent();
-
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"hosts" ofType:@"txt"];
-
     mk::ooni::TcpConnectTest()
-    .set_port("80")
-    .set_input_file_path([path UTF8String])
-    .set_verbose()
-    .on_log([self](const char *s) {
-        NSString *current = [NSString stringWithFormat:@"%@: %@", [super getDate],
-                             [NSString stringWithUTF8String:s]];
-        NSLog(@"%s", s);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self.logLines addObject:current];
+        .set_options("port", "80")
+        .set_options("dns/nameserver", get_dns_server())
+        .set_input_file_path([path UTF8String])
+        .set_verbosity(MK_LOG_DEBUG2)
+        .on_log([self](uint32_t, const char *s) {
+            NSString *current = [NSString stringWithFormat:@"%@: %@", [super getDate],
+                                 [NSString stringWithUTF8String:s]];
+            NSLog(@"%s", s);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.logLines addObject:current];
+            });
+        })
+        .run([self]() {
+            NSLog(@"tcp_connect testEnded");
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.finished = TRUE;
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:nil];
+            });
         });
-    })
-    .run([self]() {
-        NSLog(@"tcp_connect testEnded");
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.finished = TRUE;
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"refreshTable" object:nil];
-        });
-    });
 }
 
 @end
