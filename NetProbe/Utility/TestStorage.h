cdcd96240450b1e8dfc5e0c91f55baa0f795cb83
lorenzoPrimi <lprimiterra@yahoo.it>
2016-06-11 19:32:16 +0200

    TestStorage added

---
+++ NetProbe/Utility/TestStorage.h
@@ -0,0 +1,13 @@
+//
+//  TestStorage.h
+//  NetProbe
+//
+//  Created by Lorenzo Primiterra on 11/06/16.
+//  Copyright © 2016 Simone Basso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestStorage : NSObject
+
+@end
