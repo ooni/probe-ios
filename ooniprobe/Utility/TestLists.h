2a1b050fc08ecd3ec3c18dd4ae50ca38c1cf722f
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-10-07 19:38:26 -0400

    Created class to read csv and produce arrays of urls
    Only Russian for test purposes

---
+++ ooniprobe/Utility/TestLists.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface TestLists : NSObject
+
++ (NSArray*)getUrls;
+
+@end
---
+++ ooniprobe/Utility/TestLists.mm
@@ -0,0 +1,29 @@
+#import "TestLists.h"
+#import "NTYCSVTable.h"
+#include <measurement_kit/ooni.hpp>
+
+@implementation TestLists
+
++ (NSArray*)getUrls{
+    NSMutableArray *urls;
+    NSArray *global_urls = [self getUrlsForCountry:@"global"];
+    [urls addObjectsFromArray:global_urls];
+    NSArray *local_urls = [self getUrlsForCountry:@"ru"];
+    if (local_urls != nil)
+        [urls addObjectsFromArray:local_urls];
+    NSLog(@"url %@", urls);
+    return urls;
+}
+
++ (NSArray*)getUrlsForCountry:(NSString*)country_code {
+    if ([[NSBundle mainBundle] pathForResource:country_code ofType:@"csv"] != nil){
+        NSURL *csvURL = [NSURL URLWithString:[NSString stringWithFormat:@"file://%@", [[NSBundle mainBundle] pathForResource:country_code ofType:@"csv"]]];
+        NTYCSVTable *table = [[NTYCSVTable alloc] initWithContentsOfURL:csvURL];
+        NSArray *url = table.columns[@"url"];
+        return url;
+    }
+    return nil;
+}
+
+
+@end
