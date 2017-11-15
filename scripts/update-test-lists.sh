c4d29d1ac7077cb71306fc006d3fdadcc0da80fc
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-11-15 19:36:19 +0100

    Indent script

--- scripts/update-test-lists.sh
+++ scripts/update-test-lists.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-for i in $(ls ooniprobe/Resources/test-lists/); 
-do wget -q "https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/$i" -O "ooniprobe/Resources/test-lists/$i";
+for i in $(ls ooniprobe/Resources/test-lists/); do 
+  wget -q \
+    "https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/$i" \
+      -O "ooniprobe/Resources/test-lists/$i";
 done
