2510d8261affc5bd9f96adf2e985f5ce69838db5
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2017-11-15 18:56:01 +0100

    Wrap line

--- scripts/update-test-lists.sh
+++ scripts/update-test-lists.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-for i in $(ls ooniprobe/Resources/test-lists/); do wget -q "https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/$i" -O "ooniprobe/Resources/test-lists/$i"; done
+for i in $(ls ooniprobe/Resources/test-lists/); 
+do wget -q "https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/$i" -O "ooniprobe/Resources/test-lists/$i";
+done
