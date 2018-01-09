b4877f69007aa44dfcd26063b67d083ec920b4a6
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-01-09 13:20:16 +0700

    Language updated

--- scripts/update_languages.sh
+++ scripts/update_languages.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -u
-tx pull
-tx pull -l zh_CN
-tx pull -l zh_TW
+tx pull -f
+tx pull -f -l zh_CN
+tx pull -f -l zh_TW
 cp ooniprobe/zh_CN.lproj/Localizable.strings ooniprobe/zh-Hans.lproj/Localizable.strings
 cp ooniprobe/zh_TW.lproj/Localizable.strings ooniprobe/zh-Hant.lproj/Localizable.strings
 rm -Rf ooniprobe/zh_CN.lproj ooniprobe/zh_TW.lproj
