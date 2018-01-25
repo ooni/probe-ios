046f697537cbec239a84468f52e3350ea676b104
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-01-25 16:51:04 +0700

    Removed laws_text_5

--- ooniprobe/View/Informed consent/LawsViewController.m
+++ ooniprobe/View/Informed consent/LawsViewController.m
@@ -23,7 +23,7 @@
     
     [self.titleLabel setText:NSLocalizedString(@"potential_risks", nil)];
     [self.nextButton setTitle:[NSString stringWithFormat:@"   %@   ", NSLocalizedString(@"i_understand_the_risks", nil)] forState:UIControlStateNormal];
-    [self.textLabel setText:[NSString stringWithFormat:@"%@\n%@\n%@\n%@\n%@\n%@",NSLocalizedString(@"laws_text_1", nil),  NSLocalizedString(@"laws_text_2", nil), NSLocalizedString(@"laws_text_3", nil), NSLocalizedString(@"laws_text_4", nil), NSLocalizedString(@"laws_text_5", nil), NSLocalizedString(@"laws_text_6", nil)]];
+    [self.textLabel setText:[NSString stringWithFormat:@"%@\n%@\n%@\n%@\n%@",NSLocalizedString(@"laws_text_1", nil),  NSLocalizedString(@"laws_text_2", nil), NSLocalizedString(@"laws_text_3", nil), NSLocalizedString(@"laws_text_4", nil), NSLocalizedString(@"laws_text_6", nil)]];
     [self.moreButton setTitle:[NSString stringWithFormat:@"%@", NSLocalizedString(@"learn_more", nil)] forState:UIControlStateNormal];
 }
 
