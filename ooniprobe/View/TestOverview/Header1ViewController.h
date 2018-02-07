0850a62eea4922f7daa0395230d6278ebf65ccee
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-02-07 10:59:23 +0800

    Setting header

--- ooniprobe/View/TestOverview/Header1ViewController.h
+++ ooniprobe/View/TestOverview/Header1ViewController.h
@@ -3,5 +3,17 @@
 @interface Header1ViewController : UIViewController
 
 @property (nonatomic, strong) NSString *testName;
+@property (nonatomic, strong) IBOutlet UIStackView *view1;
+@property (nonatomic, strong) IBOutlet UILabel *label1;
+@property (nonatomic, strong) IBOutlet UILabel *label1text;
+@property (nonatomic, strong) IBOutlet UIStackView *view2;
+@property (nonatomic, strong) IBOutlet UILabel *label2;
+@property (nonatomic, strong) IBOutlet UILabel *label2text;
+@property (nonatomic, strong) IBOutlet UIStackView *view3;
+@property (nonatomic, strong) IBOutlet UILabel *label3;
+@property (nonatomic, strong) IBOutlet UILabel *label3text;
+@property (nonatomic, strong) IBOutlet UIStackView *view4;
+@property (nonatomic, strong) IBOutlet UILabel *label4;
+@property (nonatomic, strong) IBOutlet UILabel *label4text;
 
 @end
--- ooniprobe/View/TestOverview/Header1ViewController.m
+++ ooniprobe/View/TestOverview/Header1ViewController.m
@@ -8,7 +8,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self addLine:self.view2];
+    [self addLine:self.view3];
+    [self addLine:self.view4];
+
+/*
+ UIView *LineView=[[UIView alloc]initWithFrame:CGRectMake(self.view.frame.size.width-5, 0, 1, self.view.frame.size.height)]; // customize the frame what u need
+ [LineView setBackgroundColor:[UIColor whiteColor]]; //customize the color
+ [self.view1 addSubview:LineView];
+ 
+
+    CGFloat sortaPixel = 1 / [UIScreen mainScreen].scale;
+    
+    UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, sortaPixel)];
+    
+    line.userInteractionEnabled = NO;
+    line.backgroundColor = self.backgroundColor;
+    
+    line.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    
+    [self.view1 addSubview:line];
+    
+    self.backgroundColor = [UIColor clearColor];
+    self.userInteractionEnabled = NO;
+*/
 }
 
+-(void)addLine:(UIView*)view{
+    UIView *lineView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 1, view.frame.size.height)];
+    [lineView setBackgroundColor:[UIColor whiteColor]];
+    [view addSubview:lineView];
+}
 @end
