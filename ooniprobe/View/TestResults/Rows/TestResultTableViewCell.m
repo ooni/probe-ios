6817da0f57a8c81bee4008a5c96234e6e717cbca
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-02-22 10:37:59 +0800

    Set result row images and text

--- ooniprobe/View/TestResults/Rows/TestResultTableViewCell.m
+++ ooniprobe/View/TestResults/Rows/TestResultTableViewCell.m
@@ -42,6 +42,46 @@
     //from https://developer.apple.com/library/content/documentation/MacOSX/Conceptual/BPInternational/InternationalizingLocaleData/InternationalizingLocaleData.html
     NSString *localizedDateTime = [NSDateFormatter localizedStringFromDate:result.startTime dateStyle:NSDateFormatterShortStyle timeStyle:NSDateFormatterShortStyle];
     self.testTimeLabel.text = localizedDateTime;
+    if ([result.name isEqualToString:@"websites"]){
+        [self.stackView2 setHidden:NO];
+        [self.stackView3 setHidden:YES];
+        [self.image1 setImage:[UIImage imageNamed:@"red_x"]];
+        [self.label1 setText:@"1 blocked"];
+        [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
+        [self.image2 setImage:[UIImage imageNamed:@"globe_black"]];
+        [self.label2 setText:@"10 tested"];
+        [self.label2 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
+    }
+    else if ([result.name isEqualToString:@"instant_messaging"]){
+        [self.stackView2 setHidden:NO];
+        [self.stackView3 setHidden:YES];
+        [self.image1 setImage:[UIImage imageNamed:@"red_x"]];
+        [self.label1 setText:@"1 blocked"];
+        [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
+        [self.image2 setImage:[UIImage imageNamed:@"green_v"]];
+        [self.label2 setText:@"2 available"];
+        [self.label2 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
+    }
+    else if ([result.name isEqualToString:@"middle_boxes"]){
+        [self.stackView2 setHidden:YES];
+        [self.stackView3 setHidden:YES];
+        [self.image1 setImage:nil];
+        [self.label1 setText:NSLocalizedString(@"found", nil)];
+        [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_yellow8 alpha:1.0f]];
+    }
+    else if ([result.name isEqualToString:@"performance"]){
+        [self.stackView2 setHidden:NO];
+        [self.stackView3 setHidden:NO];
+        [self.image1 setImage:[UIImage imageNamed:@"upload_black"]];
+        [self.label1 setText:@"900kbps"];
+        [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
+        [self.image2 setImage:[UIImage imageNamed:@"download_black"]];
+        [self.label2 setText:@"10kbps"];
+        [self.label2 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
+        [self.image3 setImage:[UIImage imageNamed:@"video_quality_black"]];
+        [self.label3 setText:@"1080p"];
+        [self.label3 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
+    }
 }
 
 @end
