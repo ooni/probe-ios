b219a6347e2ff9f2175d29c322faca808c77c9e5
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-09-11 15:43:53 -0500

    update setting only on confirmation

--- ooniprobe/Utility/MessageUtility.h
+++ ooniprobe/Utility/MessageUtility.h
@@ -6,5 +6,6 @@
 + (void)showToast:(NSString*)msg inView:(UIView*)view;
 + (void)alertWithTitle:(NSString *)title message:(NSString *)msg inView:(UIViewController *)view;
 + (void)alertWithTitle:(NSString *)title message:(NSString *)msg okButton:(UIAlertAction*)okButton inView:(UIViewController *)view;
++ (void)alertWithTitle:(NSString *)title message:(NSString *)msg okButton:(UIAlertAction*)okButton cancelButton:(UIAlertAction*)cancelButton inView:(UIViewController *)view;
 + (void)notificationAlertinView:(UIViewController *)view;
 @end
