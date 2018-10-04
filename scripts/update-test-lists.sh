dfd119b11b470874a14564bd07ae2acada71f106
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-10-04 12:42:57 -0500

    clean scripts

--- scripts/update-test-lists.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(ls ooniprobe/Resources/test-lists/); do 
-  wget -q \
-    "https://raw.githubusercontent.com/citizenlab/test-lists/master/lists/$i" \
-      -O "ooniprobe/Resources/test-lists/$i";
-done
