03b87974796bba828d67afbcac19c9eb708cc938
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-10-21 16:10:07 -0500

    composed strings

--- ooniprobe/View/TestResults/Rows/TestResultTableViewCell.m
+++ ooniprobe/View/TestResults/Rows/TestResultTableViewCell.m
@@ -1,4 +1,5 @@
 #import "TestResultTableViewCell.h"
+#import "GRMustache.h"
 
 @implementation TestResultTableViewCell
 
@@ -41,11 +42,11 @@
         [self.stackView3 setHidden:YES];
         [self.image1 setImage:[UIImage imageNamed:@"cross"]];
         [self.image1 setTintColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
-        [self.label1 setText:[NSString stringWithFormat:@"%ld %@", anomalousMeasurements, [LocalizationUtility getSingularPlural:anomalousMeasurements :@"TestResults.Overview.Websites.Blocked"]]];
+        [self.label1 setText:[NSString stringWithFormat:@"%@", [LocalizationUtility getSingularPlural:anomalousMeasurements :@"TestResults.Overview.Websites.Blocked"]]];
         [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
         [self.image2 setImage:[UIImage imageNamed:@"globe"]];
         [self.image2 setTintColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
-        [self.label2 setText:[NSString stringWithFormat:@"%ld %@", totalMeasurements, [LocalizationUtility getSingularPlural:totalMeasurements :@"TestResults.Overview.Websites.Tested"]]];
+        [self.label2 setText:[NSString stringWithFormat:@"%@", [LocalizationUtility getSingularPlural:totalMeasurements :@"TestResults.Overview.Websites.Tested"]]];
         [self.label2 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
     }
     else if ([result.test_group_name isEqualToString:@"instant_messaging"]){
@@ -55,11 +56,11 @@
         [self.stackView3 setHidden:YES];
         [self.image1 setImage:[UIImage imageNamed:@"cross"]];
         [self.image1 setTintColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
-        [self.label1 setText:[NSString stringWithFormat:@"%ld %@", anomalousMeasurements, [LocalizationUtility getSingularPlural:anomalousMeasurements :@"TestResults.Overview.InstantMessaging.Blocked"]]];
+        [self.label1 setText:[NSString stringWithFormat:@"%@", [LocalizationUtility getSingularPlural:anomalousMeasurements :@"TestResults.Overview.InstantMessaging.Blocked"]]];
         [self.label1 setTextColor:[UIColor colorWithRGBHexString:color_red8 alpha:1.0f]];
         [self.image2 setImage:[UIImage imageNamed:@"tick"]];
         [self.image2 setTintColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
-        [self.label2 setText:[NSString stringWithFormat:@"%ld %@", okMeasurements, [LocalizationUtility getSingularPlural:okMeasurements :@"TestResults.Overview.InstantMessaging.Available"]]];
+        [self.label2 setText:[NSString stringWithFormat:@"%@", [LocalizationUtility getSingularPlural:okMeasurements :@"TestResults.Overview.InstantMessaging.Available"]]];
         [self.label2 setTextColor:[UIColor colorWithRGBHexString:color_black alpha:1.0f]];
     }
     else if ([result.test_group_name isEqualToString:@"middle_boxes"]){
