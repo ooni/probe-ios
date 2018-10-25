2b140d6034cce59d547c70731aca51c5b99513f7
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-10-25 15:21:53 -0500

    onboarding start revamp

--- ooniprobe/View/Onboarding/Onboarding1ViewController.m
+++ ooniprobe/View/Onboarding/Onboarding1ViewController.m
@@ -8,9 +8,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.titleLabel setText:NSLocalizedString(@"Onboarding.Welcome.Title", nil)];
+    [self.view setBackgroundColor:[UIColor colorWithRGBHexString:color_blue5 alpha:1.0f]];
+
+    //Constraint for iPhoneSE
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+    {
+        CGSize result = [[UIScreen mainScreen] bounds].size;
+        if(result.height == 568)
+        {
+            self.topConstraint.constant = 8.0f;
+        }
+    }
+
+    self.nextButton.layer.cornerRadius = 30;
+    self.nextButton.layer.masksToBounds = true;
+    
+    [self.nextButton setTitle:NSLocalizedString(@"Onboarding.WhatIsOONIProbe.GotIt", nil) forState:UIControlStateNormal];
+     [self.nextButton setTitleColor:[UIColor colorWithRGBHexString:color_blue8 alpha:1.0f]
+                           forState:UIControlStateNormal];
+     [self.nextButton setBackgroundColor:[UIColor whiteColor]];
+
     [self.titleLabel setTextColor:[UIColor whiteColor]];
-    [self.view setBackgroundColor:[UIColor colorWithRGBHexString:color_base alpha:1.0f]];
+    [self.titleLabel setText:NSLocalizedString(@"Onboarding.WhatIsOONIProbe.Title", nil)];
+    /*
+    NSMutableAttributedString *whatIsOoniprobe1 = [[NSMutableAttributedString alloc] initWithString:NSLocalizedString(@"Onboarding.WhatIsOONIProbe.Paragraph.1", nil)];
+    [whatIsOoniprobe1 addAttribute:NSFontAttributeName
+                    value:[UIFont fontWithName:@"FiraSans-SemiBold" size:17]
+                    range:NSMakeRange(0, whatIsOoniprobe1.length)];
+    
+    NSMutableAttributedString *whatIsOoniprobe2 = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"\n\n%@", NSLocalizedString(@"Onboarding.WhatIsOONIProbe.Paragraph.2", nil)]];
+    [whatIsOoniprobe2 addAttribute:NSFontAttributeName
+                    value:[UIFont fontWithName:@"FiraSans-Regular" size:17]
+                    range:NSMakeRange(0, whatIsOoniprobe2.length)];
+    
+    NSMutableAttributedString *whatIsOoniprobe3 = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"\n\n%@", NSLocalizedString(@"Onboarding.WhatIsOONIProbe.Paragraph.3", nil)]];
+    [whatIsOoniprobe3 addAttribute:NSFontAttributeName
+                             value:[UIFont fontWithName:@"FiraSans-Regular" size:17]
+                             range:NSMakeRange(0, whatIsOoniprobe3.length)];
+
+    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] init];
+    [attrStr appendAttributedString:whatIsOoniprobe1];
+    [attrStr appendAttributedString:whatIsOoniprobe2];
+    [attrStr appendAttributedString:whatIsOoniprobe3];
+    [self.textLabel setAttributedText:attrStr];
+    */
+    [self.textLabel setText:NSLocalizedString(@"Onboarding.WhatIsOONIProbe.Paragraph", nil)];
+    [self.textLabel setTextColor:[UIColor whiteColor]];
 }
+
 
 @end
--- ooniprobe/View/Onboarding/Onboarding3ViewController.h
+++ ooniprobe/View/Onboarding/Onboarding3ViewController.h
@@ -1,19 +1,14 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
-#import "PopQuizViewController.h"
 
-@interface Onboarding3ViewController : UIViewController <PopQuizViewControllerDelegate> {
-    UIView *quizView;
-    UIView *wrongAnswerView;
-}
+@interface Onboarding3ViewController : UIViewController
 
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
 @property (strong, nonatomic) IBOutlet UILabel *textLabel;
-@property (strong, nonatomic) IBOutlet UIButton *nextButton;
-@property (nonatomic, assign) int question_number;
+
+@property (strong, nonatomic) IBOutlet UIButton *changeButton;
+@property (strong, nonatomic) IBOutlet UIButton *goButton;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *topConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
-
--(void)setQuestion_number:(int)qn;
 
 @end
