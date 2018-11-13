38686ce819a62fd0182b2170f4844ff51ef13c51
Arturo Filastò <arturo@filasto.net>
2018-11-13 12:55:23 +0100

    Make some tweaks to the animations

--- ooniprobe/View/TestResults/Rows/DashboardTableViewCell.m
+++ ooniprobe/View/TestResults/Rows/DashboardTableViewCell.m
@@ -15,13 +15,25 @@
 - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated{
     [super setHighlighted:highlighted animated:animated];
     if (highlighted) {
-        [UIView animateWithDuration:0.15f animations:^{
-            self.contentView.alpha = 0.5f;
-        }];
+        [UIView
+            animateWithDuration:0.3f
+            delay:0
+            options:UIViewAnimationOptionCurveEaseOut
+            animations:^{
+                self.contentView.alpha = 0.7f;
+            }
+            completion: NULL
+         ];
     } else {
-        [UIView animateWithDuration:0.35f animations:^{
-            self.contentView.alpha = 1.f;
-        }];
+        [UIView
+         animateWithDuration:0.8f
+         delay:0.5f
+         options:UIViewAnimationOptionCurveEaseInOut
+         animations:^{
+             self.contentView.alpha = 1.f;
+         }
+         completion: NULL
+        ];
     }
 }
 
