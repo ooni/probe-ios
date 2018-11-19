9f01171b59b8d500ed44c499672559ef63aeb968
Arturo Filastò <arturo@filasto.net>
2018-11-19 18:55:18 +0100

    Update the translation update script

--- scripts/update_languages.sh
+++ scripts/update_languages.sh
@@ -1,8 +1,25 @@
-#! /bin/bash -u
-tx pull -f
-tx pull -f -l zh_CN
-tx pull -f -l zh_TW
-cp ooniprobe/zh_CN.lproj/Localizable.strings ooniprobe/zh-Hans.lproj/Localizable.strings
-cp ooniprobe/zh_TW.lproj/Localizable.strings ooniprobe/zh-Hant.lproj/Localizable.strings
-rm -Rf ooniprobe/zh_CN.lproj ooniprobe/zh_TW.lproj
-rm -Rf description
+#!/bin/bash
+set -e
+
+TOPDIR=$(cd $(dirname $0)/.. && pwd -P)
+
+if [ ! -d ooniprobe/ ];then
+    echo "script must be run from the root of the repo"
+    exit 1
+fi
+
+if [ ! -d ../translations/probe-mobile ];then
+    echo "error: you should clone https://github.com/ooni/translations in ../"
+    exit 1
+fi
+
+cd ../translations
+./update-translations.sh
+for dir in probe-mobile/*/;do
+    lang=$(basename ${dir} | tr 'zh_CN' 'zh-Hans' | tr 'zh_TW' 'zh-Hant')
+    dst_path="${TOPDIR}/ooniprobe/${lang}.lproj/"
+    mkdir -p $dst_path
+    cp ${dir}Localizable.strings $dst_path
+done
+echo "Translations have been updated"
+echo "Remember to commit the changes made to ooni/translations!"
