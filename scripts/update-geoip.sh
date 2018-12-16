b6c7e2887cf51314ff0e32031fbff642524f31c1
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2018-12-16 16:38:39 +0700

    removed old scripts

--- scripts/update-geoip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-base=https://download.maxmind.com/download/geoip/database
-wget $base/GeoLiteCountry/GeoIP.dat.gz -O GeoIP.dat.gz
-gzip -d GeoIP.dat.gz
-wget $base/asnum/GeoIPASNum.dat.gz -O GeoIPASNum.dat.gz
-gzip -d GeoIPASNum.dat.gz
-mv GeoIP.dat GeoIPASNum.dat ../ooniprobe/Resources/
