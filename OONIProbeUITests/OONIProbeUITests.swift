d863c3382816d53e8a3e2367417628213a6fa907
Arturo Filastò <arturo@filasto.net>
2019-01-03 17:01:22 +1300

    Get the fixture copy logic to work

--- OONIProbeUITests/OONIProbeUITests.swift
+++ OONIProbeUITests/OONIProbeUITests.swift
@@ -14,7 +14,52 @@ class OONIProbeUITests: XCTestCase {
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        let repoRoot = URL(fileURLWithPath: #file).deletingLastPathComponent().deletingLastPathComponent()
+        let fixturesDir = repoRoot.appendingPathComponent("fixtures")
 
+        if let simulatorSharedDir = ProcessInfo().environment["SIMULATOR_SHARED_RESOURCES_DIRECTORY"] {
+            let appID = UIDevice.current.identifierForVendor!.uuidString
+
+            //7E5DBB2A-6BE3-4102-995E-5A2280445854/Documents/OONIProbe.db
+            
+            // running on the simulator. We'll write to ~/Library/Caches
+            let simulatorHomeDirURL = URL(fileURLWithPath: simulatorSharedDir)
+            let containerDir = simulatorHomeDirURL.appendingPathComponent("Containers/Data/Application")
+            
+            let appDirs = try! FileManager.default.contentsOfDirectory(atPath: containerDir.path)
+
+            // XXX maybe a better way to do this is:
+            // https://developer.apple.com/documentation/foundation/filemanager/1412643-containerurl
+            var targetAppDir = containerDir
+            for appDir in appDirs {
+                targetAppDir = containerDir.appendingPathComponent(appDir)
+                if FileManager.default.fileExists(atPath: targetAppDir.appendingPathComponent("Documents/OONIProbe.db").path) {
+                    print("Success!!")
+                    print(targetAppDir)
+                    break
+                }
+            }
+            
+            XCTAssertTrue(targetAppDir != containerDir, "Could not find targetAppDir")
+
+            let prefPath = targetAppDir.appendingPathComponent("Library/Preferences/org.openobservatory.ooniprobe.plist")
+            let dbPath = targetAppDir.appendingPathComponent("Documents/OONIProbe.db")
+            try? FileManager.default.removeItem(at: prefPath)
+            try! FileManager.default.copyItem(
+                atPath: fixturesDir.appendingPathComponent("org.openobservatory.ooniprobe.plist").path,
+                toPath: targetAppDir.appendingPathComponent("Library/Preferences/org.openobservatory.ooniprobe.plist").path
+            )
+            try? FileManager.default.removeItem(at: dbPath)
+            try! FileManager.default.copyItem(
+                atPath: fixturesDir.appendingPathComponent("OONIProbe.db").path,
+                toPath: targetAppDir.appendingPathComponent("Documents/OONIProbe.db").path
+            )
+            print("repo Root \(repoRoot)\n")
+            print("targetAppDir \(targetAppDir.path)\n")
+            print("sharedFolderURL \(containerDir.path)\n")
+            print("appID: \(appID)\n")
+        }
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
         setupSnapshot(app)
