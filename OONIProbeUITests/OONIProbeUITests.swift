687ae0981e794759e1a6a48ab0009eacaa4df312
Arturo Filastò <arturo@filasto.net>
2019-01-07 17:14:44 +1300

    Remove hardcoded german language from unittests

--- OONIProbeUITests/OONIProbeUITests.swift
+++ OONIProbeUITests/OONIProbeUITests.swift
@@ -60,9 +60,6 @@ class OONIProbeUITests: XCTestCase {
         
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
-        app.launchArguments += ["-AppleLanguages", "(de)"]
-        app.launchArguments += ["-AppleLocale", "de_DE"]
-
         setupSnapshot(app)
         app.launch()
 
