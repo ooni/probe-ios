d090d962776fffd2320d7dde1ff199f85e93a76c
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-02-17 13:20:58 +0700

    nulling result on end of test

--- ooniprobe/Test/Suite/AbstractSuite.mm
+++ ooniprobe/Test/Suite/AbstractSuite.mm
@@ -44,6 +44,7 @@
     //if last test
     if ([self.testList count] == 0){
         [self.result setIs_done:YES];
+        self.result = nil;
         UIApplicationState state = [[UIApplication sharedApplication] applicationState];
         if (state == UIApplicationStateBackground || state == UIApplicationStateInactive){
             if ([SettingsUtility getSettingWithName:@"notifications_enabled"] && [SettingsUtility getSettingWithName:@"notifications_completion"])
@@ -69,10 +70,13 @@
 }
 
 -(void)newResult {
-    self.result = [Result new];
-    [self.result setTest_group_name:self.name];
-    //The Result object needs to be saved to have an Id, needed for log
-    [self.result save];
+    //For rerun measurement result is set by the view controller
+    if (self.result == nil){
+        self.result = [Result new];
+        [self.result setTest_group_name:self.name];
+        //The Result object needs to be saved to have an Id, needed for log
+        [self.result save];
+    }
 }
 
 -(int)getRuntime{
