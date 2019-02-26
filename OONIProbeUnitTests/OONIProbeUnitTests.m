2057f24b780eceddb434d767f87609c409044df7
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-02-26 20:23:54 +0700

    added unit test

---
+++ OONIProbeUnitTests/OONIProbeUnitTests.m
@@ -0,0 +1,37 @@
+//
+//  OONIProbeUnitTests.m
+//  OONIProbeUnitTests
+//
+//  Created by Lorenzo Primiterra on 26/02/2019.
+//  Copyright © 2019 OONI. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OONIProbeUnitTests : XCTestCase
+
+@end
+
+@implementation OONIProbeUnitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
