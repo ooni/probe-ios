f769b7c038a83d24db4913fd5f1518c56418865c
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-03-14 16:29:59 +0800

    tewak to failed screen

--- ooniprobe/View/TestResults/Details/FailedTestDetailsViewController.m
+++ ooniprobe/View/TestResults/Details/FailedTestDetailsViewController.m
@@ -1,4 +1,5 @@
 #import "FailedTestDetailsViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface FailedTestDetailsViewController ()
 
@@ -16,6 +17,10 @@
     [self.titleLabel setText:NSLocalizedString(@"TestResults.Details.Failed.Title", nil)];
     [self.subtitleLabel setText:NSLocalizedString(@"TestResults.Details.Failed.Paragraph", nil)];
     [self.tryAgainButton setTitle:NSLocalizedString(@"TestResults.Details.Failed.TryAgain", nil) forState:UIControlStateNormal];
+    self.tryAgainButton.layer.cornerRadius = self.tryAgainButton.bounds.size.height/2;
+    self.tryAgainButton.layer.masksToBounds = YES;
+    self.tryAgainButton.layer.borderWidth = 0.5f;
+    self.tryAgainButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
 }
 
 
