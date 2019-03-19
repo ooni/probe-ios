3cb279e78405d18cdb835e681497b3aa90a6691f
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-03-19 21:38:29 +0300

    new colors in header

--- ooniprobe/View/TestResults/Details/FailedTestDetailsViewController.m
+++ ooniprobe/View/TestResults/Details/FailedTestDetailsViewController.m
@@ -11,12 +11,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.titleLabel setText:self.measurement.url_id.url];
-    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRGBHexString:color_yellow5 alpha:1.0f]];
-    [self.statusImage setTintColor:[UIColor colorWithRGBHexString:color_gray9 alpha:1.0f]];
-    [self.headerView setBackgroundColor:[UIColor colorWithRGBHexString:color_yellow5 alpha:1.0f]];
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRGBHexString:color_gray7 alpha:1.0f]];
+    [self.statusImage setTintColor:[UIColor colorWithRGBHexString:color_white alpha:1.0f]];
+    [self.headerView setBackgroundColor:[UIColor colorWithRGBHexString:color_gray7 alpha:1.0f]];
     [self.statusImage setImage:[UIImage imageNamed:@"error"]];
-    [self.titleLabel setTextColor:[UIColor colorWithRGBHexString:color_gray9 alpha:1.0f]];
-    [self.subtitleLabel setTextColor:[UIColor colorWithRGBHexString:color_gray9 alpha:1.0f]];
+    [self.titleLabel setTextColor:[UIColor colorWithRGBHexString:color_white alpha:1.0f]];
+    [self.subtitleLabel setTextColor:[UIColor colorWithRGBHexString:color_white alpha:1.0f]];
     [self.titleLabel setText:NSLocalizedString(@"TestResults.Details.Failed.Title", nil)];
     [self.subtitleLabel setText:NSLocalizedString(@"TestResults.Details.Failed.Paragraph", nil)];
     [self.tryAgainButton setTitle:NSLocalizedString(@"TestResults.Details.Failed.TryAgain", nil) forState:UIControlStateNormal];
