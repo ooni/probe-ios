cbe4c7fa3bce4032587e55dc77c649c7f07af2a3
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-03-20 22:04:25 +0300

    requested fixes

--- ooniprobe/Utility/UIDevice-DeviceInfo.m
+++ ooniprobe/Utility/UIDevice-DeviceInfo.m
@@ -7,14 +7,17 @@
 {
     size_t size = 0;
     int ret = sysctlbyname(typeSpecifier, NULL, &size, NULL, 0);
-    if (ret == 0 || size == 0) abort();
+    if (ret != 0 || size == 0) abort();
 
     char *answer = malloc(size);
     if (answer == NULL) abort();
     
-    int ret2 = sysctlbyname(typeSpecifier, answer, &size, NULL, 0);
-    if (ret2 == 0) abort();
-
+    ret = sysctlbyname(typeSpecifier, answer, &size, NULL, 0);
+    if (ret != 0) {
+        free(answer);
+        abort();
+    }
+    
     NSString *results = [NSString stringWithCString:answer encoding: NSUTF8StringEncoding];
 
     free(answer);
