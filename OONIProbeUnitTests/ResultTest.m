b860e5b90fcdb0435b8bace2e15a8ed4202b7ab2
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-04-17 18:40:02 +0200

    split into multiple tests

--- OONIProbeUnitTests/ResultTest.m
+++ OONIProbeUnitTests/ResultTest.m
@@ -8,42 +8,50 @@
 
 @implementation ResultTest
 Result *result;
+Measurement *measurement;
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
     [[[Result query] fetch] remove];
     result = [Result new];
+    measurement = [Measurement new];
+    measurement.result_id = result;
+    [measurement save];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testNotUploaded {
+- (void)testNoMeasurements {
     //No measurement, isEveryMeasurementUploaded true
-    XCTAssert([result isEveryMeasurementUploaded]);
-    Measurement *measurement = [Measurement new];
-    measurement.result_id = result;
-    [measurement save];
+    XCTAssert([[Result new] isEveryMeasurementUploaded]);
+}
+
+- (void)testNotUploaded {
     //One measurement, isEveryMeasurementUploaded false
     XCTAssert(![result isEveryMeasurementUploaded]);
+}
+
+- (void)testUploaded {
+    //Measurement uploaded but report_id null, isEveryMeasurementUploaded false
     measurement.is_uploaded = true;
     [measurement save];
-    //Measurement uploaded but report_id null, isEveryMeasurementUploaded false
     XCTAssert(![result isEveryMeasurementUploaded]);
+}
+
+- (void)testUploadedAndReport {
+    //Measurement uploaded and wirh report_id, isEveryMeasurementUploaded true
+    measurement.is_uploaded = true;
     measurement.report_id = REPORT_ID;
     [measurement save];
-    //Measurement uploaded and wirh report_id, isEveryMeasurementUploaded true
     XCTAssert([result isEveryMeasurementUploaded]);
-    
-    Measurement *measurement2 = [Measurement new];
-    measurement2.result_id = result;
-    [measurement2 save];
-    //Second measurement not uploaded, isEveryMeasurementUploaded false
-    XCTAssert(![result isEveryMeasurementUploaded]);
-    measurement2.is_failed = true;
-    [measurement2 save];
+}
+
+- (void)testFailed {
     //Second measurement failed, isEveryMeasurementUploaded true
+    measurement.is_failed = true;
+    [measurement save];
     XCTAssert([result isEveryMeasurementUploaded]);
 }
 
