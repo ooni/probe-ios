9d79b0d4eac485667de7609cdfb44a38c669cfed
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-04-20 12:08:30 +0200

    documentation

--- ooniprobe/View/TestResults/Footer/UploadFooterViewController.h
+++ ooniprobe/View/TestResults/Footer/UploadFooterViewController.h
@@ -2,17 +2,19 @@
 #import "Result.h"
 #import "Measurement.h"
 
-///This view controller manage the upload footer and its method. It's contained as sub view in 3 other views
+/// This view controller manage the upload footer and its method. It's contained as sub view in 3 other views
 @interface UploadFooterViewController : UIViewController
 
-/// result and/or measurement contains the Result and/or the Measurement that the upload footer is about to upload
+/// result contains the Result that the upload footer is about to upload
 @property (nonatomic, strong) Result *result;
+
+/// measurement contains the the Measurement that the upload footer is about to upload
 @property (nonatomic, strong) Measurement *measurement;
 
 @property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property (nonatomic, strong) IBOutlet UIButton *uploadButton;
 
-//upload_all is a bool used to indicate if the footer should upload a measurement (one) or a result (all)
+// upload_all is a bool used to indicate if the footer should upload a measurement (one) or a result (all)
 @property (assign) bool upload_all;
 
 @end
