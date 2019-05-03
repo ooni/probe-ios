2c3d7079577dbd28c0010e9ba75569fcc7273791
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-05-03 14:59:33 +0200

    right wat to prevent IC

--- OONIProbeUITests/OONIProbeUITests.swift
+++ OONIProbeUITests/OONIProbeUITests.swift
@@ -63,12 +63,11 @@ class OONIProbeUITests: XCTestCase {
             print("appID: \(appID)\n")
         }
         
-        //Preventing Informed Consent to appear
-        UserDefaults.standard.set("ok", forKey: "first_run")
-
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
         setupSnapshot(app)
+        //Preventing Informed Consent to appear
+        app.launchArguments += ["-first_run", "ok"]
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
