e1c188d1af0ae8ba50b4388d341ea514418af3a8
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-05-04 16:22:02 +0200

    fix for the 2.1 version

--- OONIProbeUITests/OONIProbeUITests.swift
+++ OONIProbeUITests/OONIProbeUITests.swift
@@ -15,6 +15,7 @@ class OONIProbeUITests: XCTestCase {
         // Solution taken rom https://blog.codecentric.de/en/2018/06/resetting-ios-application-state-userdefaults-ui-tests/
         //Preventing Informed Consent to appear
         app.launchArguments += ["-first_run", "ok"]
+        app.launchArguments += ["-upload_result_manually_popup", "ok"]
         app.launchArguments += ["UI-Testing"]
         app.launch()
 
