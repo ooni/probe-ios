c746539fbaba0b1cf6fb6db5849584ba18eb841a
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2019-07-03 13:33:49 -0500

    refactor methods

--- ooniprobe/View/TestResults/LogViewController.m
+++ ooniprobe/View/TestResults/LogViewController.m
@@ -15,33 +15,41 @@
                                             style:UIBarButtonItemStylePlain
                                             target:self
                                             action:@selector(copy_clipboard:)];
-
-    if ([self.type isEqualToString:@"log"] || [self.type isEqualToString:@"json"]){
-        NSString *fileName;
-        if ([self.type isEqualToString:@"log"])
-            fileName = [self.measurement getLogFile];
-        else if ([self.type isEqualToString:@"json"])
-            fileName = [self.measurement getReportFile];
+    NSString *fileName;
+    if ([self.type isEqualToString:@"log"]){
+        fileName = [self.measurement getLogFile];
         NSString *content = [TestUtility getUTF8FileContent:fileName];
         if (content != nil) {
-            if ([self.type isEqualToString:@"json"]){
-                NSError *error;
-                NSData *jsonData = [content dataUsingEncoding:NSUTF8StringEncoding];
-                id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
-                if (error != nil) return;
-                NSData *prettyJsonData = [NSJSONSerialization dataWithJSONObject:jsonObject options:NSJSONWritingPrettyPrinted error:&error];
-                if (error != nil) return;
-                NSString *prettyPrintedJson = [NSString stringWithUTF8String:[prettyJsonData bytes]];
-                [self.textView setText:prettyPrintedJson];
-            }
-            else
-                [self.textView setText:content];
+            [self.textView setText:content];
+        }
+        //TODO show some Log not found
+    }
+    else if ([self.type isEqualToString:@"json"]){
+        fileName = [self.measurement getReportFile];
+        NSString *content = [TestUtility getUTF8FileContent:fileName];
+        if (content != nil) {
+            NSString *prettyPrintedJson = [self prettyPrintedJson:content];
+            [self.textView setText:prettyPrintedJson];
+        }
+        else {
+            //TODO Download content from web
         }
     }
     [self.textView setTextColor:[UIColor colorWithRGBHexString:color_gray9 alpha:1.0f]];
     self.textView.scrollEnabled = false;
     [self.textView layoutIfNeeded];
     self.textView.scrollEnabled = true;
+}
+
+-(NSString*)prettyPrintedJson:(NSString *)content{
+    NSError *error;
+    NSData *jsonData = [content dataUsingEncoding:NSUTF8StringEncoding];
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+    if (error != nil) return content;
+    NSData *prettyJsonData = [NSJSONSerialization dataWithJSONObject:jsonObject options:NSJSONWritingPrettyPrinted error:&error];
+    if (error != nil) return content;
+    NSString *prettyPrintedJson = [NSString stringWithUTF8String:[prettyJsonData bytes]];
+    return prettyPrintedJson;
 }
 
 -(IBAction)copy_clipboard:(id)sender{
