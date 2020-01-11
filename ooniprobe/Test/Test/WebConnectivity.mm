1e3d1d3bbef6cc8f33269f86040fbb30d20bc6bb
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-01-11 16:53:42 +0700

    Comment

--- ooniprobe/Test/Test/WebConnectivity.mm
+++ ooniprobe/Test/Test/WebConnectivity.mm
@@ -48,6 +48,9 @@
  if the option max_runtime is already set in the option and is not MAX_RUNTIME_DISABLED let's use it
  else if the input are sets we calculate 5 seconds per input
  at last we check if max_runtime is enabled, in  that case we use the value in the settings
+ 
+ first two cases : test is already running and with options and/or URL s
+ last two cases : get max_runtime saved in the preference
  */
 -(int)getRuntime{
     if (self.settings.options.max_runtime != nil &&
