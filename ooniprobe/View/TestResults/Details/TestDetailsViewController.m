657572313c4f6d77b04f5fc19ed9ba298342ad68
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-01-13 12:48:48 +0700

    Revert "Not deleting log file when measurement is uploaded"
    This reverts commit 9b078b57fb17b3451622ab727fec82935ab75657.

--- ooniprobe/View/TestResults/Details/TestDetailsViewController.m
+++ ooniprobe/View/TestResults/Details/TestDetailsViewController.m
@@ -30,6 +30,7 @@
         [self.measurement getExplorerUrl:^(NSString *measurement_url){
             isInExplorer = TRUE;
             [TestUtility removeFile:[self.measurement getReportFile]];
+            [TestUtility removeFile:[self.measurement getLogFile]];
         } onError:^(NSError *error) {
             isInExplorer = FALSE;
         }];
