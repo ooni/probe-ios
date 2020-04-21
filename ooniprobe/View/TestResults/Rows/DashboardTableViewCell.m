649b0e0d6bf3f40d485da729fffc8154c14f1759
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-04-21 10:12:26 +0200

    Add gradient to test icons

--- ooniprobe/View/TestResults/Rows/DashboardTableViewCell.m
+++ ooniprobe/View/TestResults/Rows/DashboardTableViewCell.m
@@ -43,9 +43,8 @@
 -(void)setTestSuite:(AbstractSuite*)testSuite{
     [self.titleLabel setText:[LocalizationUtility getNameForTest:testSuite.name]];
     [self.descLabel setText:[LocalizationUtility getDescriptionForTest:testSuite.name]];
-    //TODO add icon _gradient
-    [self.testLogo setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%@", testSuite.name]]];
-    [self.testLogo setTintColor:[TestUtility getColorForTest:testSuite.name]];
+    //TODO set right colors
+    [self.testLogo setImage:[self imageWithGradient:[UIImage imageNamed:[NSString stringWithFormat:@"%@", testSuite.name]] startColor:[UIColor whiteColor] endColor:[TestUtility getColorForTest:testSuite.name]]];
     [self.cardbackgroundView setBackgroundColor:[UIColor whiteColor]];
 }
 
@@ -65,4 +64,31 @@
     self.contentView.layer.masksToBounds = NO;
 }
 
+//From https://stackoverflow.com/questions/8098130/how-can-i-tint-a-uiimage-with-gradient
+-(UIImage *)imageWithGradient:(UIImage *)img startColor:(UIColor *)color1 endColor:(UIColor *)color2 {
+    UIGraphicsBeginImageContextWithOptions(img.size, NO, img.scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, 0, img.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+
+    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    CGRect rect = CGRectMake(0, 0, img.size.width, img.size.height);
+    //CGContextDrawImage(context, rect, img.CGImage);
+
+    // Create gradient
+    NSArray *colors = [NSArray arrayWithObjects:(id)color2.CGColor, (id)color1.CGColor, nil];
+    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColors(space, (__bridge CFArrayRef)colors, NULL);
+
+    // Apply gradient
+    CGContextClipToMask(context, rect, img.CGImage);
+    CGContextDrawLinearGradient(context, gradient, CGPointMake(0,0), CGPointMake(0, img.size.height), 0);
+    UIImage *gradientImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(space);
+
+    return gradientImage;
+}
 @end
