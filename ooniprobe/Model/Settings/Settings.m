e2adc09749444bbd94e3428d1f8cdac536a19e20
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-04-27 20:48:35 +0200

    Check include_asn setting before sending network_type

--- ooniprobe/Model/Settings/Settings.m
+++ ooniprobe/Model/Settings/Settings.m
@@ -8,7 +8,9 @@
 - (id)init {
     self = [super init];
     if (self) {
-        self.annotations = [[NSMutableDictionary alloc] initWithDictionary:@{@"network_type" : [[ReachabilityManager sharedManager] getStatus]}];
+        BOOL include_asn = [SettingsUtility getSettingWithName:@"include_asn"];
+        if (include_asn)
+            self.annotations = [[NSMutableDictionary alloc] initWithDictionary:@{@"network_type" : [[ReachabilityManager sharedManager] getStatus]}];
         self.disabled_events = @[@"status.queued", @"status.update.websites", @"failure.report_close"];
         self.log_level = [SettingsUtility getVerbosity];
         self.options = [Options new];
