6ca57a5dbfa86d3dbc0dedeac66bc818b4293947
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2020-04-29 16:23:32 +0200

    Remove old keys

--- ooniprobe/Model/JsonResult/Advanced.h
+++ ooniprobe/Model/JsonResult/Advanced.h
@@ -2,9 +2,7 @@
 
 @interface Advanced : NSObject
 @property (nonatomic, strong) NSNumber *packet_loss;
-@property (nonatomic, strong) NSNumber *out_of_order;
 @property (nonatomic, strong) NSNumber *avg_rtt;
 @property (nonatomic, strong) NSNumber *max_rtt;
 @property (nonatomic, strong) NSNumber *mss;
-@property (nonatomic, strong) NSNumber *timeouts;
 @end
