22585441d4d1b0c4f273d5a6372627a98318d81a
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-02-03 10:48:27 +0000

    Crash traking

--- ooniprobe/AppDelegate.m
+++ ooniprobe/AppDelegate.m
@@ -8,6 +8,7 @@
 #import "CountlyUtility.h"
 #import "ReachabilityManager.h"
 @import Firebase;
+@import Sentry;
 
 @interface AppDelegate ()
 
@@ -30,9 +31,17 @@
     [[UIBarButtonItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: [UIFont fontWithName:@"FiraSans-Regular" size:16],NSFontAttributeName, nil] forState:UIControlStateNormal];
     [NavigationBarUtility setDefaults];
     [CountlyUtility initCountly];
-#ifdef RELEASE
+    
+//#ifdef RELEASE
     [FIRApp configure];
-#endif
+    [SentrySDK startWithConfigureOptions:^(SentryOptions *options) {
+        options.dsn = @"https://fc33135e184e402aa805fa48cd65f0a5@o155150.ingest.sentry.io/5619986";
+        //If the DSN is nil or empty or enabled is set to false, the client won't send any data to Sentry.
+        options.enabled = [SettingsUtility isSendCrashEnabled];
+        options.debug = @YES; // Enabled debug when first installing is always helpful
+    }];
+//#endif
+    
     //Init the ReachabilityManager singleton
     [ReachabilityManager sharedManager];
     application.statusBarStyle = UIStatusBarStyleLightContent;
--- ooniprobe/Utility/ExceptionUtility.m
+++ ooniprobe/Utility/ExceptionUtility.m
@@ -1,14 +1,24 @@
 #import "ExceptionUtility.h"
 #import "SettingsUtility.h"
 @import Firebase;
+@import Sentry;
 
 @implementation ExceptionUtility
 
 + (void)recordError:(NSString*)title reason:(NSString*)reason userInfo:(NSDictionary*)userInfo{
+    //Preparing NSError for Firebase
     NSError *error = [NSError errorWithDomain:title
                                          code:-1001
                                      userInfo:userInfo];
     [[FIRCrashlytics crashlytics] recordError:error];
+
+    //Preparing NSException for Sentry
+    NSException* exception = [NSException exceptionWithName:title
+                                                         reason:reason
+                                                       userInfo:userInfo];
+    //TODO which one to use?
+    [SentrySDK captureException:exception];
+    [SentrySDK captureError:error];
 }
 
 @end
