df77424323cacafbf60cf7dd9e96780ec6e98ea2
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-04-15 11:42:33 +0100

    Update AppDelegate.m

--- ooniprobe/AppDelegate.m
+++ ooniprobe/AppDelegate.m
@@ -37,7 +37,6 @@
     [SettingsUtility incrementAppOpenCount];
     
     [BackgroundTask configure];
-    //[BackgroundTask checkIn];
 
     return YES;
 }
@@ -71,6 +70,8 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    if ([SettingsUtility isAutomatedTestEnabled])
+        [BackgroundTask scheduleCheckIn];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
