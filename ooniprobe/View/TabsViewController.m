b50c1781d4e8fdbdaae22865327a08c9a31641d3
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-06-14 14:26:06 +0100

    New logic popup

--- ooniprobe/View/TabsViewController.m
+++ ooniprobe/View/TabsViewController.m
@@ -22,16 +22,16 @@
         [self performSegueWithIdentifier:@"showInformedConsent" sender:self];
     }
     if ([SettingsUtility getAppOpenCount] != 0 &&
-        [SettingsUtility getAppOpenCount] % NOTIFICATION_POPUP_COUNT == 0 &&
+       [SettingsUtility getAppOpenCount] % NOTIFICATION_POPUP_COUNT == 0 &&
+       ![SettingsUtility isAutomatedTestEnabled] &&
+       ![[NSUserDefaults standardUserDefaults] objectForKey:NOTIFICATION_AUTOTEST_DISABLE]){
+       [MessageUtility autotestAlertinView:self];
+    }
+    else if ([SettingsUtility getAppOpenCount] != 0 &&
+        [SettingsUtility getAppOpenCount] % NOTIFICATION_AUTOTEST_COUNT == 0 &&
         ![SettingsUtility isNotificationEnabled] &&
         ![[NSUserDefaults standardUserDefaults] objectForKey:NOTIFICATION_POPUP_DISABLE]){
         [MessageUtility notificationAlertinView:self];
-    }
-    else if ([SettingsUtility getAppOpenCount] != 0 &&
-        [SettingsUtility getAppOpenCount] % NOTIFICATION_POPUP_COUNT == 0 &&
-        ![SettingsUtility isAutomatedTestEnabled] &&
-        ![[NSUserDefaults standardUserDefaults] objectForKey:NOTIFICATION_AUTOTEST_DISABLE]){
-        [MessageUtility autotestAlertinView:self];
     }
 }
 
