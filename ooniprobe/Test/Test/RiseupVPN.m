58468eeb28a6d81eccd2cbe96eda8bd330edf479
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-11-17 10:19:13 -0300

    Unifying keys

--- ooniprobe/Test/Test/RiseupVPN.m
+++ ooniprobe/Test/Test/RiseupVPN.m
@@ -16,7 +16,7 @@
 }
 
 -(void)onEntry:(JsonResult*)json obj:(Measurement*)measurement{
-    bool isTransportBlocked = [json.test_keys.transport_status[@"openvpn"]  isEqual: @"blocked"] || [json.test_keys.transport_status[@"obfs4"]  isEqual: @"blocked"];
+    bool isTransportBlocked = [json.test_keys.transport_status[@"openvpn"]  isEqualToString:BLOCKED] || [json.test_keys.transport_status[@"obfs4"]  isEqualToString:BLOCKED];
 
     measurement.is_anomaly = !json.test_keys.ca_cert_status.boolValue || json.test_keys.api_failure != nil || isTransportBlocked;
     [super onEntry:json obj:measurement];
