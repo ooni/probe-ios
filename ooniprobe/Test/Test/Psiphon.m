9e43ca37dab402e926617741a92977ddfdbe453e
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-11-29 12:31:03 -0300

    Unifying is_failed code like Android app

--- ooniprobe/Test/Test/Psiphon.m
+++ ooniprobe/Test/Test/Psiphon.m
@@ -16,6 +16,10 @@
 }
 
 -(void)onEntry:(JsonResult*)json obj:(Measurement*)measurement{
+    if (json.test_keys == NULL) {
+        measurement.is_failed = true;
+        return;
+    }
     measurement.is_anomaly = json.test_keys.failure == NULL ? false : true;
     [super onEntry:json obj:measurement];
 }
--- ooniprobe/Test/Test/RiseupVPN.m
+++ ooniprobe/Test/Test/RiseupVPN.m
@@ -16,8 +16,11 @@
 }
 
 -(void)onEntry:(JsonResult*)json obj:(Measurement*)measurement{
+    if (json.test_keys == NULL || json.test_keys.transport_status == NULL) {
+        measurement.is_failed = true;
+        return;
+    }
     bool isTransportBlocked = [json.test_keys.transport_status[@"openvpn"]  isEqualToString:BLOCKED] || [json.test_keys.transport_status[@"obfs4"]  isEqualToString:BLOCKED];
-
     measurement.is_anomaly = !json.test_keys.ca_cert_status.boolValue || json.test_keys.api_failure != nil || isTransportBlocked;
     [super onEntry:json obj:measurement];
 
