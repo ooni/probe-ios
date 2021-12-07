39c00cd890b764247a5e5eb2a6ffa3d0cd7ee42f
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-12-07 11:17:44 -0300

    run InstantMessagingSuite and CircumventionSuite with autorun

--- ooniprobe/Utility/BackgroundTask.m
+++ ooniprobe/Utility/BackgroundTask.m
@@ -7,6 +7,7 @@
 #import "Suite.h"
 #import "Tests.h"
 #import "ReachabilityManager.h"
+#import "RunningTest.h"
 
 @implementation BackgroundTask
 
@@ -65,6 +66,8 @@
     if ([[ReachabilityManager sharedManager] isVPNConnected])
         return;
     
+    NSMutableArray *tests = [[NSMutableArray alloc] init];
+
     NSString *testName = @"web_connectivity";
     NSString *testSuiteName = [TestUtility getCategoryForTest:testName];
     AbstractSuite *testSuite = [[AbstractSuite alloc] initSuite:testSuiteName];
@@ -73,6 +76,7 @@
     [test setStoreDB:NO];
     [test setAnnotation:YES];
     [testSuite setTestList:[NSMutableArray arrayWithObject:test]];
+    [tests addObject:testSuite];
 
     [OONIApi checkIn:^(NSArray *urls) {
         if ([urls count] == 0)
@@ -85,7 +89,15 @@
     }];
     [SettingsUtility incrementAutorun];
     [SettingsUtility updateAutorunDate];
-    [testSuite runTestSuite];
+    
+    InstantMessagingSuite *imTest = [[InstantMessagingSuite alloc] init];
+    [imTest setStoreDB:NO];
+    [tests addObject:imTest];
+    CircumventionSuite *cTest = [[CircumventionSuite alloc] init];
+    [cTest setStoreDB:NO];
+    [tests addObject:cTest];
+    [[RunningTest currentTest] setAndRun:[NSMutableArray arrayWithArray:tests]];
+    //[testSuite runTestSuite];
 }
 
 + (void)cancelCheckIn API_AVAILABLE(ios(13.0)){
