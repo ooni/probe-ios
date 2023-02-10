a6cefe19b96c5e11de151b4ddfbee4a70aee2ca2
Norbel Ambanumben <aanorbel@gmail.com>
2023-02-10 11:28:01 +0100

    updated performance suite background tests

--- ooniprobe/Test/Suite/PerformanceSuite.m
+++ ooniprobe/Test/Suite/PerformanceSuite.m
@@ -14,13 +14,13 @@
 
 - (NSArray*)getTestList {
     if ([self.testList count] == 0){
-        if ([SettingsUtility getSettingWithName:@"run_ndt"])
+        if ([SettingsUtility getSettingWithName:@"run_ndt"]  && !self.autoRun)
             [self.testList addObject:[[NdtTest alloc] init]];
-        if ([SettingsUtility getSettingWithName:@"run_dash"])
+        if ([SettingsUtility getSettingWithName:@"run_dash"]  && !self.autoRun)
             [self.testList addObject:[[Dash alloc] init]];
-        if ([SettingsUtility getSettingWithName:@"run_http_invalid_request_line"] && !self.autoRun)
+        if ([SettingsUtility getSettingWithName:@"run_http_invalid_request_line"])
             [self.testList addObject:[[HttpInvalidRequestLine alloc] init]];
-        if ([SettingsUtility getSettingWithName:@"run_http_header_field_manipulation"] && !self.autoRun)
+        if ([SettingsUtility getSettingWithName:@"run_http_header_field_manipulation"])
             [self.testList addObject:[[HttpHeaderFieldManipulation alloc] init]];
     }
     return super.getTestList;
