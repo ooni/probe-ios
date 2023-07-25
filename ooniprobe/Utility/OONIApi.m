bfff43e3bddad7118b4157c2154ec80214538778
Norbel Ambanumben <aanorbel@gmail.com>
2023-07-25 19:12:42 +0100

    Disables `WebsitesSuite` if no category is enabled. Also prevent checkin call if no category is enabled

--- ooniprobe/Utility/OONIApi.m
+++ ooniprobe/Utility/OONIApi.m
@@ -21,6 +21,12 @@
     if (error != nil) {
         return;
     }
+    
+    if ([SettingsUtility getNumberCategoriesEnabled] <= 0){
+        errorcb(error);
+        return;
+    }
+    
     OONIContext *ooniContext = [session newContextWithTimeout:30];
     OONICheckInConfig *config = [[OONICheckInConfig alloc] initWithSoftwareName:SOFTWARE_NAME
                                                                 softwareVersion:[VersionUtility get_software_version]
